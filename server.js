2eb38f72d3e49b66c73aef57010b9d823339882e
Lu asik bang <98279739+NdaaaBilek@users.noreply.github.com>
2023-05-21 23:01:29 +0700

    Update server.jss

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 var express = require("express")
+var scr = require("api-dylux")
 var app = express()
-var db = require("./database.js")
-var md5 = require("md5")
-
-var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 var HTTP_PORT = 8000
 
@@ -14,114 +9,33 @@ app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
 
-app.get("/api/users", (req, res, next) => {
-    var sql = "select * from user"
-    var params = []
-    db.all(sql, params, (err, rows) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":rows
-        })
-      });
-});
-
-
-app.get("/api/user/:id", (req, res, next) => {
-    var sql = "select * from user where id = ?"
-    var params = [req.params.id]
-    db.get(sql, params, (err, row) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":row
-        })
-      });
-});
-
-
-app.post("/api/user/", (req, res, next) => {
-    var errors=[]
-    if (!req.body.password){
-        errors.push("No password specified");
-    }
-    if (!req.body.email){
-        errors.push("No email specified");
-    }
-    if (errors.length){
-        res.status(400).json({"error":errors.join(",")});
-        return;
-    }
-    var data = {
-        name: req.body.name,
-        email: req.body.email,
-        password : md5(req.body.password)
-    }
-    var sql ='INSERT INTO user (name, email, password) VALUES (?,?,?)'
-    var params =[data.name, data.email, data.password]
-    db.run(sql, params, function (err, result) {
-        if (err){
-            res.status(400).json({"error": err.message})
-            return;
-        }
-        res.json({
-            "message": "success",
-            "data": data,
-            "id" : this.lastID
-        })
-    });
+app.get("/xnxxsearch", async (req, res, next) => {
+    let query = req.query.query
+    if (!query) return res.json({"message":"masukkan query"})
+    const result = await scr.xnxxSearch(query)
+    res.json({
+        result
+    })
+})
+app.get("/xnxxdl", async (req, res, next) => {
+    let url = req.query.url
+    if (!url) return res.json({"message":"masukkan url"})
+    const result = await scr.xnxxdl(url)
+    res.json({
+        result
+    })
+})
+app.get("tiktok", async (req, res, next) => {
+    let url = req.query.url
+    if (!url) return res.json({"message":"masukkan url"})
+    const result = await scr.tiktok(url)
+    res.json({
+        result
+    })
 })
 
-
-
-app.patch("/api/user/:id", (req, res, next) => {
-    var data = {
-        name: req.body.name,
-        email: req.body.email,
-        password : req.body.password ? md5(req.body.password) : undefined
-    }
-    db.run(
-        `UPDATE user set 
-           name = coalesce(?,name), 
-           email = COALESCE(?,email), 
-           password = coalesce(?,password) 
-           WHERE id = ?`,
-        [data.name, data.email, data.password, req.params.id],
-        (err, result) => {
-            if (err){
-                res.status(400).json({"error": res.message})
-                return;
-            }
-            res.json({
-                message: "success",
-                data: data
-            })
-    });
-})
-
-
-app.delete("/api/user/:id", (req, res, next) => {
-    db.run(
-        'DELETE FROM user WHERE id = ?',
-        req.params.id,
-        function (err, result) {
-            if (err){
-                res.status(400).json({"error": res.message})
-                return;
-            }
-            res.json({"message":"deleted", rows: this.changes})
-    });
-})
-
-
-// Root path
 app.get("/", (req, res, next) => {
     res.json({"message":"Ok"})
 });
 
+s
