c74672cc3597cbb03a5c259d3f1789076696b283
Lu asik bang <98279739+NdaaaBilek@users.noreply.github.com>
2023-05-24 23:28:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,29 @@ var HTTP_PORT = 8000
 app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
-
+app.get("/ai", async (req, res, next) => {
+    const { Configuration, OpenAIApi } = require("openai")
+    let query = req.query.query
+    if (!query) return res.json({"message":"masukkan query"})
+        const keynya = "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"  //Input your OpenAI api-Key -> https://beta.openai.com/account/api-keys
+const configuration = new Configuration({
+  apiKey: keynya,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.createCompletion({
+              model: "text-davinci-003",
+              prompt: query,
+              temperature: 0,
+              max_tokens: 1000,
+              top_p: 1,
+              frequency_penalty: 0.2,
+              presence_penalty: 0,
+            });
+            const resultnya = response.data.choices[0].text;
+            res.json({
+                resultnya
+            })
+})
 app.get("/xnxxsearch", async (req, res, next) => {
     let query = req.query.query
     if (!query) return res.json({"message":"masukkan query"})
